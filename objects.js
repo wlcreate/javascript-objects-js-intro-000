2db4cf1e016db970b0fe63b107bd17c1c824e7a4
Waverley Leung <waverley.nyc@gmail.com>
2020-06-07 20:42:22 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+//Assign an object to the var playlist and initialize the object with a key-value pair
+//the keys will be artist names and the values will be song titles
+var playlist = {artistName: "songTitle" }
+
+//three parameters: the playlist (an object), an artist name (a string), and a song title
+//body of the function should add the song and artist as a key-value pair to the playlist object
+//function should return the whole playlist
+function updatePlaylist(playlist, artistName, songTitle) {
+  Object.assign({}, playlist, {[Phil Ochs]: "Here's to the State of Mississippi"})
+  return playlist
+}
